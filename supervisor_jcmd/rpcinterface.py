402bf10f317354cd9e8a0609d5b9ba58f2d172c1
Jamie Bliss <jamie.bliss@ilq.com>
2017-09-18 17:47:43 -0400

    First blind stab

--- supervisor_jcmd/rpcinterface.py
+++ supervisor_jcmd/rpcinterface.py
@@ -1,10 +1,14 @@
+from supervisor.options import split_namespec
 from supervisor.states import SupervisorStates
 from supervisor.xmlrpc import Faults
 from supervisor.xmlrpc import RPCError
 
-API_VERSION = '0.2'
+import subprocess
 
-class CacheNamespaceRPCInterface:
+API_VERSION = '0.1'
+
+
+class JcmdNamespaceRPCInterface:
     """ A Supervisor RPC interface that provides the ability
     to cache abritrary data in the Supervisor instance as key/value pairs.
     """
@@ -14,14 +18,11 @@ class CacheNamespaceRPCInterface:
         self.cache = {}
 
     def _update(self, text):
-        self.update_text = text # for unit tests, mainly
+        self.update_text = text  # for unit tests, mainly
 
-        state = self.supervisord.get_state()
-
-        if state == SupervisorStates.SHUTDOWN:
+        if isinstance(self.supervisord.options.mood, int) and \
+           self.supervisord.options.mood < SupervisorStates.RUNNING:
             raise RPCError(Faults.SHUTDOWN_STATE)
-
-        # XXX fatal state
 
     # RPC API methods
 
@@ -33,75 +34,48 @@ class CacheNamespaceRPCInterface:
         self._update('getAPIVersion')
         return API_VERSION
 
-    def getKeys(self):
-        """ Return keys for all data stored in the cache
+    def _getGroupAndProcess(self, name):
+        # get process to start from name
+        group_name, process_name = split_namespec(name)
 
-        @return  array   An array of strings representing cache keys
-        """
-        self._update('getKeys')
-        return sorted(self.cache.keys())
+        group = self.supervisord.process_groups.get(group_name)
+        if group is None:
+            raise RPCError(Faults.BAD_NAME, name)
 
-    def getCount(self):
-        """ Return a count of all items in the cache
+        if process_name is None:
+            return group, None
 
-        @return  integer   Count of items
-        """
-        self._update('getCount')
-        return len(self.cache)
+        process = group.processes.get(process_name)
+        if process is None:
+            raise RPCError(Faults.BAD_NAME, name)
 
-    def store(self, key, data):
+        return group, process
+
+    def jcmd(self, name, cmd):
         """ Store a string value in the cache, referenced by 'key'
 
-        @param  string key   A string to use as a cache key
-        @param  string data  A string for cache value
-        @return boolean      Always true unless error
+        @param  array cmd  A list of strings to give to jcmd
+        @return struct     Results of the command
         """
-        self._update('store')
-        self._validateKey(key)
+        self._update(cmd)
 
-        self.cache[key] = data
-        return True
+        _, process = self._getGroupAndProcess(name)
 
-    def fetch(self, key):
-        """ Retrieve data from cache stored under 'key'
+        # TODO: Raise an error if it's not RUNNING
 
-        @param  string key  The cache key
-        @return string      Cache data stored at key
-        """
-        self._update('fetch')
-        self._validateKey(key)
+        pid = process.pid
 
-        if key not in self.cache:
-            raise RPCError(Faults.BAD_NAME)
-        return self.cache[key]
+        proc = subprocess.Popen(['jcmd', 'pid'] + list(cmd),
+            stdin=subprocess.PIPE, stdout=subprocess.PIPE,
+            stderr=subprocess.STDOUT)
 
-    def delete(self, key):
-        """ Delete data stored in cache under 'key'
+        out, _ = proc.communicate()
 
-        @param  string  key  The key to delete from the cache
-        @return boolean      Always true unless error.
-        """
-        self._update('delete')
-        self._validateKey(key)
+        return {
+            'status': proc.returncode,
+            'output': out,
+        }
 
-        if key in self.cache:
-            del self.cache[key]
-        return True
 
-    def clear(self):
-        """ Clear the cache
-
-        @return boolean  Always true unless error.
-        """
-        self._update('clear')
-        self.cache.clear()
-        return True
-
-    def _validateKey(self, key):
-        """ validate 'key' is suitable for a cache key name """
-        if not isinstance(key, str) or (key == ''):
-            why = 'Cache key must be a non-empty string'
-            raise RPCError(Faults.BAD_NAME, why)
-
-def make_cache_rpcinterface(supervisord, **config):
-    return CacheNamespaceRPCInterface(supervisord)
+def make_rpcinterface(supervisord, **config):
+    return JcmdNamespaceRPCInterface(supervisord)
